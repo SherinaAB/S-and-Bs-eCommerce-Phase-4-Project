aaca77a0804bb6e5d1d949117eca840412727488
Sherina Buenaseda <sherina76@gmail.com>
2023-08-30 14:47:52 -0600

    added User Login and Authentication

--- server/app.py
+++ server/app.py
@@ -1,21 +1,195 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-
+from flask import Flask
 # Remote library imports
-from flask import request
-from flask_restful import Resource
-
+from flask import request, make_response, jsonify, session
+from flask_restful import Api, Resource
+# from flask_login import LoginManager, login_required, current_user
 # Local imports
 from config import app, db, api
+from models import db, User, Product, Cart_Item, Shopping_Session, User_Payment
 # Add your model imports
 
 
 # Views go here!
 
+# login_manager = LoginManager(app)
+# login_manager.login_view = 'login'
+
+# users = current_user.get_id()
+
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
+
+class Users(Resource):
+    def get(self):
+        users = [u.to_dict() for u in User.query.all()]
+        return make_response(users, 200)
+    
+    def post(self):
+
+        fields = request.get_json()
+
+        try:
+            username = fields['username']
+            password = fields['password']
+            email = fields['email']
+            if username and password and email:
+                new_user = User(username=username, password=password, email=email)
+                new_user.password_hash = password
+                
+                db.session.add(new_user)
+                db.session.commit()
+
+                session['user_id'] = new_user.id
+            
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 422)
+        return make_response(new_user.to_dict(), 201)
+    
+    def delete(self, id):
+        user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({"errors": ["user not found"]}, 404)
+        
+class UserById(Resource):
+    def get(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+
+        if user_by_id == None:
+            user_by_id_dict = {"errors": ["user not found"]}
+            return make_response(user_by_id_dict, 404)
+        else:
+            return make_response(user_by_id.to_dict(), 200)
+    
+    def patch(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+
+        if user_by_id == None:
+            user_by_id_dict = {"errors": ["user not found"]}
+
+            return make_response(user_by_id_dict, 404)
+        else: 
+            user_by_id = request.get_json()
+            try:
+                for attr in user_by_id():
+                    setattr(user_by_id, attr, user_by_id[attr])
+                    db.session.add(user_by_id)
+                    db.session.commit()
+            except ValueError:
+                return make_response({"errors": ["validation errors"]}, 422)
+            return make_response(user_by_id.to_dict(), 200)
+    
+    def delete(self, id):
+        user_by_id = User.query.filter_by(id=id).first()
+
+        if user_by_id == None:
+            user_by_id_dict = {"errors": ["user not found"]}
+
+            return make_response(user_by_id_dict, 404)
+        else:
+            db.session.delete(user_by_id)
+            db.session.commit()
+            user_by_id_dict = {}
+            return make_response(user_by_id.to_dict(), 200)
+
+class Login(Resource):
+    def post(self):
+        login = request.get_json()
+        username = login['username']
+        password = login['password']
+        user = User.query.filter(User.username == username).first()
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+        return {'error': 'Unauthorized'}, 401
+    
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message':'204: No Content'}
+    
+class Products(Resource):
+    def get(self):
+        products = [p.to_dict() for p in Product.query.all()]
+        return make_response(products, 200)
+    
+    def post(self):
+        fields = request.get_json()
+
+        try:
+            name = fields['name']
+            desc = fields['desc']
+            sku = fields['sku']
+            copy = fields['copy']
+            price = fields['price']
+            inventory = fields['inventory']
+            if name and desc and price:
+                new_product = Product(name=name, desc=desc, sku=sku, copy=copy, price=price, inventory=inventory)
+                db.session.add(new_product)
+                db.session.commit()
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 422)
+        return make_response(new_product.to_dict(), 201)
+    
+    def patch(self, id):
+        products = Product.query.filter_by(id=id).first()
+
+        if products == None:
+            products_dict = {"errors": ["user not found"]}
+
+            return make_response(products_dict, 404)
+        else: 
+            products = request.get_json()
+            try:
+                for attr in products():
+                    setattr(products, attr, products[attr])
+                    db.session.add(products)
+                    db.session.commit()
+            except ValueError:
+                return make_response({"errors": ["validation errors"]}, 422)
+            return make_response(products.to_dict(), 200)    
+
+    def delete(self, id):
+        products = Product.query.filter_by(id=id).first()
+
+        if products == None:
+            products_dict = {"errors": ["user not found"]}
+
+            return make_response(products_dict, 404)
+        else:
+            db.session.delete(products)
+            db.session.commit()
+            products_dict = {}
+            return make_response(products.to_dict(), 200)
+        
+        
+
+        
+api.add_resource(Users, '/users')
+api.add_resource(UserById, '/users/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Products, '/products')
+
+
+        
 
 
 if __name__ == '__main__':
--- server/seed.py
+++ server/seed.py
@@ -8,11 +8,80 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db, User
+from models import db, Product, User, Cart_Item, Shopping_Session, User_Payment
 
 def create_database():
  with app.app_context():
         db.create_all()
+
+def create_products():
+    products = []
+    for _ in range(50):
+        p = Product(
+            name = fake.name(),
+            desc = fake.text(),
+            sku = fake.ean(length=13),
+            copy = fake.text(),
+            price = randint(1, 1000000),
+            inventory = randint(1, 1000000),
+            user_id = randint(1, 15),
+        )
+        products.append(p)
+    return products
+
+def create_users():
+    users = []
+    for _ in range(15):
+        u = User(
+            username = fake.name(),
+            password_hash = "123abc", 
+            first_name = fake.name(),
+            last_name = fake.name(),
+            email = fake.email(),
+            phone = fake.phone_number(),
+            role = rc(["admin", "user"]),
+            user_payment_id = randint(1, 1000000),
+        )
+        users.append(u)
+    return users
+
+def create_shopping_sessions():
+    sessions = []
+    for _ in range(15):
+        s = Shopping_Session(
+            total = randint(1, 1000000),
+            user_id = randint(1, 15),
+            cart_id = randint(1, 50),
+        )
+        sessions.append(s)
+    return sessions
+
+def create_cart_items():
+    items = []
+    for _ in range(15):
+        i = Cart_Item(
+            qty = randint(1, 100),
+            user_id = randint(1, 15),
+            product_id = randint(1, 50),
+        )
+        items.append(i)
+    return items
+
+def create_user_payments():
+    payments = []
+    for _ in range(15):
+        p = User_Payment(
+            # amount = randint(1, 1000000),
+            payment_type = rc(["credit", "debit"]),
+            provider = rc(["stripe", "paypal"]),
+            account_no = randint(14, 16),
+            expiry = fake.date_between(start_date="today",end_date="+10y"),
+            user_id = randint(1, 15),
+            )
+        payments.append(p)
+    return payments
+
+
 
 if __name__ == '__main__':
     fake = Faker()
@@ -20,15 +89,46 @@ if __name__ == '__main__':
         print("Starting seed...")
         # Seed code goes here!
         # create_database()
-        for _ in range(15):
-            u = User(
-                username = fake.name(),
-                password_hash = "123abc", 
-                first_name = fake.name(),
-                last_name = fake.name(),
-                email = fake.email(),
-                phone = fake.phone_number(),
-                role = rc(["admin", "user"]),
-            )
-            db.session.add(u)
-            db.session.commit()
+        # for _ in range(15):
+        #     u = User(
+        #         username = fake.name(),
+        #         password_hash = "123abc", 
+        #         first_name = fake.name(),
+        #         last_name = fake.name(),
+        #         email = fake.email(),
+        #         phone = fake.phone_number(),
+        #         role = rc(["admin", "user"]),
+        #     )
+        print("Clearing database...")
+        Product.query.delete()
+        User.query.delete()
+        Cart_Item.query.delete()
+        Shopping_Session.query.delete()
+        User_Payment.query.delete()
+
+        print("Seeding products...")
+        products = create_products()
+        db.session.add_all(products)
+        db.session.commit()
+
+        print("Seeding users...")
+        users = create_users()
+        db.session.add_all(users)
+        db.session.commit()
+
+        print("Seeding shopping sessions...")
+        sessions = create_shopping_sessions()
+        db.session.add_all(sessions)
+        db.session.commit()
+
+        print("Seeding cart items...")
+        items = create_cart_items()
+        db.session.add_all(items)
+        db.session.commit()
+
+        print("Seeding user payments...")
+        payments = create_user_payments()
+        db.session.add_all(payments)
+        db.session.commit()
+
+        print("Done seeding!")
